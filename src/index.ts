b1fb531bae0962ac8111e86725dadc1b4d64d82e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-28 21:09:39 +0900

    ダミーデータ作成

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+let blogPosts = [
+  {
+    id: "1",
+    title: "Blog1",
+    content: "Blog1 Posts",
+  },
+  {
+    id: "2",
+    title: "Blog2",
+    content: "Blog2 Posts",
+  },
+  {
+    id: "3",
+    title: "Blog3",
+    content: "Blog2 Posts",
+  }
+];
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
